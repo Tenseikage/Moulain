39cd2c49d443f4e20ecb6319e4bcbe603c837b22
M.petrov <michael.petrov@edu.univ-eiffel.fr>
2022-12-05 16:32:47 +0100

    💙

---
+++ point.py
@@ -0,0 +1,30 @@
+from fltk import cercle,abscisse_souris,ordonnee_souris,efface
+from random import random
+
+class Point:
+    liste_objet=[]
+    def __init__(self,pos):
+        self.pos = pos
+        self.color = 'black'
+        self.tag = str(random())
+        Point.liste_objet.append(self)
+
+    def affiche(self):
+        cercle(self.pos[0], self.pos[1], 10, couleur=self.color ,remplissage=self.color, tag='plato')
+
+    def efface(self):
+        efface(self.tag)
+
+    def update(self):
+        self.efface()
+        self.affiche()
+
+    def mouse_over(self):
+        if self.pos[0] - 10 < abscisse_souris() < self.pos[0] + 10 and self.pos[1] - 10 < ordonnee_souris() < self.pos[1] + 10:
+            self.color = 'red'
+            self.update()
+            return True
+        else:
+            self.color = 'black'
+            self.update()
+            return False
