d0924abee05c033f5a4db3e630bfe2b252dab7f1
Tenseikage <christaratibu51@gmail.com>
2022-12-12 09:31:03 +0100

    modif menu du jeu

--- menudujeu.py
+++ menudujeu.py
@@ -1,4 +1,5 @@
 import fltk as tk
+from random import choice
 
 
 
@@ -91,6 +92,23 @@ def menu():
                 buttonquitter.destroy_button()
                 return False
     tk.ferme_fenetre()
+
+def presentation(centre,taille_x,taille_y): # centre : position centrale du rectangle
+    '''Cette fonction permet d'avoir un menu du jeu''' # creation de la fenêtre
+    colors = ['blue','yellow' , 'pink', 'purple','cyan']
+    tk.rectangle(centre[0]-taille_x,centre[1]-taille_y,centre[0]+taille_x,centre[1]+taille_y,couleur= choice(colors), remplissage = choice(colors) ,
+    	      tag = 'Rectangle')
+    
+def present_text(string,centre,taille_x,taille_y):
+        a= centre[0]-taille_x
+        b= centre[1]-taille_y
+        c = centre[0]+taille_x
+        d = centre[1]+taille_y
+        tk.texte(a+(c-a)//2,b+(d-b)//2,chaine=string,taille=60,couleur= 'black', ancrage='center',
+    	      tag = 'text')
+        
+        
+
 
 
 
