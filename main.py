ca66768e6d5fddfcac240ea3c7e071f97b6bf1d9
Tenseikage <christaratibu51@gmail.com>
2022-12-12 08:42:47 +0100

    menu mis a jour

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 from affichage import *
-import point as point
+from point import *
 from plateau import creer_liste
+from menudujeu import *
+from menu_moulin import *
+import sys 
+
 
 
 cree_fenetre(1000,800)
 
 
-animation_plat(3,(400,400),80)
 
 def interaction_clavier():
     ev = donne_ev()
     tev = type_ev(ev)
-
     if tev == 'Quitte':
-        ferme_fenetre()
+        sys.exit(0)
+        
 
 
 
 def main():
-    print(creer_liste())
     while True:
         mise_a_jour()
-
-        point.update_points()
-
+        update_points()
         interaction_clavier()
 
 
 
-main()
+presentation((100,100),2000,2000)
+present_text('Jeu du moulin ',(500,200),300,100)
+if menu():
+    efface_tout()
+    animation_plat(3,(400,400),80)
+    main()
+
+    
