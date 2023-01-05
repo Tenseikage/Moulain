25ce0363709a2ebc4334480db3751fd11143fa1b
Tenseikage <christaratibu51@gmail.com>
2023-01-05 14:29:02 +0100

    💛

--- point.py
+++ point.py
@@ -17,9 +17,9 @@ class Point:
         if self.state == '':
             cercle(self.pos[0], self.pos[1], 13, couleur=self.color ,remplissage=self.color, tag=self.tag)
         elif self.state == 'n':
-                image(self.pos[0]+30,self.pos[1]+30, 'Moulain/images_jeu/oreo6.png', tag=self.tag) #78% 
+                image(self.pos[0]+30,self.pos[1]+30, 'Moulain/images_jeu/oreo_n.png', tag=self.tag) #78% 
         elif self.state == 'b':
-            image(self.pos[0]+3,self.pos[1]+3, 'Moulain/images_jeu/orero1.png', tag=self.tag)
+            image(self.pos[0]+3,self.pos[1]+3, 'Moulain/images_jeu/oreo_b.png', tag=self.tag)
 
     def efface(self):
         efface(self.tag)
